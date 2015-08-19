a4b3183a34ac029495ff3752d127a6ed1384c151
eurbs <emileeurbanek@gmail.com>
2015-08-18 18:50:32 -0700

    Lego QR Code quest and solution example

---
+++ legoqrcode/README.md
@@ -0,0 +1,10 @@
+# Lego QR Code
+
+Sure, you're supposed to build with legos, but it's pretty cool to lay them out in a pattern as well. This one just happens to be a QR code when you're done.
+
+#### Solution Ideas
+
+1. If you have the legos required, actually build it
+2. Use an online lego builder
+3. Write a program that takes in the input and has the ids mapped to images of the legos
+4. Color in a perfect grid excel spreadsheet
---
+++ legoqrcode/human_to_clue.py
@@ -0,0 +1,109 @@
+import os, sys
+from PIL import Image, ImageDraw
+
+# brick element ids
+b2x2 = 300326   # black 2x2
+b2x4 = 300126   # black 2x4
+w2x2 = 300301   # white 2x2
+w2x4 = 300101   # white 2x4
+
+def clue_to_image(clue, outfile):
+  """
+  :param list clue: The list of lists containing all variables.
+  :param string outfile: Path to output file.
+  """
+
+  qr = Image.new("RGBA", (1000, 1000), (255, 255, 255, 0))
+  qrctx = ImageDraw.Draw(qr)
+
+  y = 0
+  x = 0
+  x_off = 0   # to account for the 2x4's while indexed by length of list
+  for y in range(len(clue)):
+    for x in range(len(clue[y])):
+      x1 = (x + x_off) * 40
+      y1 = y * 40
+      y2 = ((y + 1) * 40) - 1
+
+      if clue[y][x] == b2x2:
+        x2 = ((x + x_off + 1) * 40) - 1
+        fill_color = "black"
+      elif clue[y][x] == b2x4:
+        x2 = ((x + x_off + 2) * 40) - 1
+        x_off += 1
+        fill_color = "black"
+      elif clue[y][x] == w2x2:
+        x2 = ((x + x_off + 1) * 40) - 1
+        fill_color = "white"
+      elif clue[y][x] == w2x4:
+        x2 = ((x + x_off + 2) * 40) - 1
+        x_off += 1
+        fill_color = "white"
+
+      # draw the rectangle!
+      qrctx.rectangle([(x1, y1), (x2, y2)], fill=fill_color)
+    x_off = 0
+
+  qr.save(outfile, "PNG")
+
+def clue_to_text(clue):
+  """List as lego element IDs in a text file"""
+  to_print = []
+  for row in clue:
+    for piece in row:
+      to_print.append("(#" + str(piece) + ")")
+    print " ".join(to_print)
+    to_print = []
+
+def clue_to_html(clue):
+  """Output for ntbdth_web webapp answers CSV"""
+  to_print = []
+  for row in clue:
+    for piece in row:
+      # to_print.append("&#40;&#35;" + str(piece) + "&#41;")
+      to_print.append("(#" + str(piece) + ")")
+    to_print.append(" <br>")
+  print " ".join(to_print)
+
+def get_clue():
+  """Hard coded in all its glory"""
+  row1 = [b2x4, b2x2, b2x4, b2x4, w2x2, b2x2, w2x2, w2x4, b2x2, w2x2, b2x2, b2x4, b2x4, b2x4]
+  row2 = [b2x2, w2x4, w2x2, w2x4, b2x2, w2x2, b2x2, w2x4, b2x4, w2x2, b2x2, w2x2, w2x4, w2x4, b2x2]
+  row3 = [b2x2, w2x2, b2x2, b2x4, w2x2, b2x2, w2x4, w2x2, b2x2, w2x2, b2x2, w2x2, b2x2, w2x2, b2x4, b2x2, w2x2, b2x2]
+  row4 = [b2x2, w2x2, b2x4, b2x2, w2x2, b2x2, w2x2, b2x4, w2x2, b2x4, w2x2, b2x2, w2x2, b2x4, b2x2, w2x2, b2x2]
+  row5 = [b2x2, w2x2, b2x4, b2x2, w2x2, b2x2, w2x4, b2x4, b2x4, w2x2, b2x2, w2x2, b2x2, b2x4, w2x2, b2x2]
+  row6 = [b2x2, w2x4, w2x4, w2x2, b2x2, w2x2, b2x2, w2x4, b2x2, w2x4, b2x2, w2x2, w2x4, w2x4, b2x2]
+  row7 = [b2x4, b2x4, b2x2, b2x4, w2x2, b2x2, w2x2, b2x2, w2x2, b2x2, w2x2, b2x4, b2x2, b2x4, b2x4]
+  row8 = [w2x4, w2x4, w2x4, w2x4, w2x2, b2x2, w2x2, b2x4, w2x4, w2x4, w2x4, w2x4]
+  row9 = [b2x4, b2x2, b2x4, w2x2, b2x4, b2x4, b2x2, b2x4, w2x2, b2x4, w2x2, b2x2, w2x2, b2x2, w2x2]
+  row10 = [w2x2, b2x4, b2x4, w2x2, b2x4, w2x2, b2x4, w2x4, b2x2, w2x4, b2x4, w2x2, b2x2, w2x2]
+  row11 = [b2x2, w2x2, b2x2, w2x4, w2x2, b2x2, w2x2, b2x4, w2x2, b2x2, w2x4, b2x2, w2x4, w2x2, b2x4, b2x2]
+  row12 = [w2x2, b2x2, w2x2, b2x4, w2x2, b2x2, w2x4, b2x2, w2x2, b2x4, w2x4, b2x2, w2x4, b2x2, w2x2, b2x2]
+  row13 = [b2x4, b2x4, w2x2, w2x4, b2x4, b2x4, b2x2, w2x4, b2x2, w2x2, b2x4, b2x4, b2x2]
+  row14 = [w2x2, b2x2, w2x2, b2x4, w2x2, b2x2, w2x4, b2x2, w2x2, b2x4, w2x4, w2x4, w2x4, w2x4]
+  row15 = [w2x2, b2x4, b2x2, w2x4, w2x4, w2x2, b2x4, b2x2, w2x4, b2x4, b2x2, b2x4, b2x4]
+  row16 = [b2x2, w2x2, b2x2, w2x4, b2x2, w2x2, b2x4, w2x2, b2x2, w2x2, b2x2, w2x2, b2x2, w2x2, w2x4, w2x4, b2x2]
+  row17 = [w2x4, w2x4, b2x2, b2x4, w2x2, w2x4, w2x4, w2x4, b2x2, w2x2, b2x4, b2x2, w2x2, b2x2]
+  row18 = [w2x2, b2x2, w2x2, b2x2, w2x4, w2x4, w2x4, w2x4, b2x2, w2x2, b2x2, w2x2, b2x4, b2x2, w2x2, b2x2]
+  row19 = [b2x2, w2x2, w2x4, b2x4, b2x4, b2x2, w2x2, b2x4, w2x4, b2x2, w2x2, b2x2, b2x4, w2x2, b2x2]
+  row20 = [w2x2, b2x2, w2x4, w2x2, b2x4, w2x4, w2x4, w2x2, b2x2, w2x2, b2x2, w2x4, w2x2, w2x4, b2x2]
+  row21 = [w2x4, w2x4, w2x4, w2x4, b2x2, w2x2, b2x4, w2x4, b2x4, b2x4, b2x2, b2x4]
+
+  final_list = [
+    row1, row2, row3, row4, row5, row6, row7,
+    row8, row9, row10, row11, row12, row13, row14,
+    row15, row16, row17, row18, row19, row20, row21,
+  ]
+  return final_list
+
+def main():
+
+
+  outfile = os.path.join(".", "solution_example.png")
+
+  clue_to_image(get_clue(), outfile)
+  clue_to_text(get_clue())
+  # clue_to_html(get_clue())
+
+if __name__ == '__main__':
+  main()
